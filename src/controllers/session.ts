68eabe576a5ad4eb00361d525f47bd5ce19dee5e
Mauritz Marais <mauritz303@gmail.com>
2024-06-04 16:35:03 +0200

    first commit

---
+++ src/controllers/session.ts
@@ -0,0 +1,7 @@
+// create and track a session
+
+// start session on visit or resume a session
+// session has (id, cart contents with quantities)
+// if cart contents - get products
+
+// end session after a certain time, if purchase completed
