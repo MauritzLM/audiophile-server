a77911f8479d6d5d3e4d770c3611dc7917f5e0a4
Mauritz Marais <mauritz303@gmail.com>
2024-06-04 13:24:19 +0200

    fixed import

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
 // app , port
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
