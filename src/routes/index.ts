cf10d3ccb155c57005cfa1b5ae8f1eccf73f4e61
Mauritz Marais <mauritz303@gmail.com>
2024-06-05 10:53:52 +0200

    added session routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,22 @@ import express from 'express';
 const router = express.Router();
 
 import { getProduct, getCategory, getFeatured } from "../controllers/product";
+import { createSession, resumeSession, updateSession, endSession } from '../controllers/session';
 
+// product routes
 router.get("/product/:name", getProduct);
 
 router.get("/category/:name", getCategory);
 
 router.get("/featured", getFeatured);
+
+// session routes
+router.post("/session-new", createSession);
+
+router.post("/resume", resumeSession);
+
+router.post("/session-update", updateSession);
+
+router.post("/session-end", endSession);
 
 export default router;
