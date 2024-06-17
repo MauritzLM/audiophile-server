9c1a21ea33a2b03b5d5b9af0d72fe1409342a222
Mauritz Marais <mauritz303@gmail.com>
2024-06-17 09:28:33 +0200

    added form submit route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 
 import { getProduct, getCategory, getFeatured } from "../controllers/product";
 import { createSession, resumeSession, updateSession, endSession } from '../controllers/session';
+import { paymentFormSubmit } from '../controllers/form';
 
 // product routes
 router.get("/product/:name", getProduct);
@@ -20,5 +21,8 @@ router.post("/session-resume", resumeSession);
 router.post("/session-update", updateSession);
 
 router.post("/session-end", endSession);
+
+// form routes
+router.post("/payment/submit", paymentFormSubmit);
 
 export default router;
