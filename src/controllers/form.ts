b3fb945221a1928e341d8b44aa02b642c2ddd5d2
Mauritz Marais <mauritz303@gmail.com>
2024-06-17 11:48:43 +0200

    added validation chains for billing and shipping details

--- src/controllers/form.ts
+++ src/controllers/form.ts
@@ -4,6 +4,43 @@ import { body, validationResult } from "express-validator"
 
 export const paymentFormSubmit = [
     // validation chain
+    // 1. name, email, phone number
+    // 2. address, zip code, city, country
+    // 3. e-money number, e-money pin
+    body("name", "please enter your name")
+        .notEmpty()
+        .trim()
+        .escape(),
+    body("email", "please enter your email")
+        .notEmpty()
+        .trim()
+        .isEmail()
+        .withMessage("wrong format")
+        .escape(),
+    body("phone", "please enter your phone number")
+        .notEmpty()
+        .trim()
+        .isNumeric()
+        .escape(),
+    body("address", "please enter your delivery address")
+        .notEmpty()
+        .trim()
+        .isAlphanumeric()
+        .withMessage("please enter a valid address")
+        .escape(),
+    body("zipcode", "please enter your zip code")
+        .trim()
+        .isPostalCode("any")
+        .withMessage("invalid zip code")
+        .escape(),
+    body("city", "please enter your city name")
+        .trim()
+        .notEmpty()
+        .escape(),
+    body("country", "please enter your country name")
+        .trim()
+        .notEmpty()
+        .escape(),
     async function (req: any, res: any, next: any) {
         try {
 
